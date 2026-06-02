e93cd770e518c62977afc1dc24c68544df5a995e
damien <damien.clochard@dalibo.com>
2026-06-02 10:02:34 +0000

    apply usermod/groupmod only if needed

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 VOLUME_UID=$(stat -c "%u" /pgrx)
 VOLUME_GID=$(stat -c "%g" /pgrx)
 
-# Remap pgrx user to match the volume owner
-usermod -u "$VOLUME_UID" pgrx 2>/dev/null
-groupmod -g "$VOLUME_GID" pgrx 2>/dev/null
+# If needed, remap pgrx user to match the volume owner
+[ "$(id -u pgrx)" != "$VOLUME_UID" ] && usermod  -u "$VOLUME_UID" pgrx  > /dev/null 2>&1
+[ "$(id -g pgrx)" != "$VOLUME_GID" ] && groupmod -g "$VOLUME_GID" pgrx > /dev/null 2>&1
 
 # Fix ownership of pgrx home dirs (cargo, rustup caches, etc.)
 chown -R pgrx:pgrx /usr/local/cargo /home/pgrx 2>/dev/null || true
